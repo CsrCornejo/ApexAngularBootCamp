d99d3f487e3d23d4a2a3d6d9f76fca9635809717
CsrCornejo <cadalcito@gmail.com>
2024-05-01 03:02:53 -0600

    Add basic item list components, update mock data

--- src/app/app-data/items/item.data.ts
+++ src/app/app-data/items/item.data.ts
@@ -54,6 +54,231 @@ export class ItemData {
       ],
       description: 'Nature focus wonder behind magazine pattern degree far without tree consider.',
     },
-    
+    {
+      id: 'item5',
+      title: 'Fjallraven - Foldsack',
+      prices: {
+        'large': 109.95
+      },
+      photos: [
+        "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      ],
+      description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    },
+    {
+      id: 'item6',
+      title: 'Mens Casual Premium Slim Fit T-Shirts',
+      prices: {
+        'small': 14.1,
+        'medium': 15.6,
+        'large': 22.3
+      },
+      photos: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      ],
+      description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    },
+    {
+      id: 'item7',
+      title: 'Solid Gold Petite Micropave',
+      prices: {
+        'small': 14.1,
+        'medium': 15.6,
+        'large': 22.3
+      },
+      photos: [
+        "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+      ],
+      description: 'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.',
+    },
+    {
+      id: 'item8',
+      title: 'Pierced Owl Rose Gold Plated Stainless Steel Double',
+      prices: {
+        '20x20': 133.35,
+        '30x12': 232.12,
+      },
+      photos: [
+        "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+        "https://api.slingacademy.com/public/sample-photos/6.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/9.jpeg",
+      ],
+      description: 'Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel',
+    },
+    {
+      id: 'item9',
+      title: 'Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5',
+      prices: {
+        '256GB': 256.00,
+        '356GB': 300.89,
+        '873GB': 500.89,
+        '900GB': 999.09,
+      },
+      photos: [
+        "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+        "https://api.slingacademy.com/public/sample-photos/5.jpeg",
+      ],
+      description: '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.',
+    },
+    {
+      id: 'item10',
+      title: 'Rain Jacket Women Windbreaker Striped Climbing Raincoats',
+      prices: {
+        'UniSize': 50.89,
+      },
+      photos: [
+        "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+      ],
+      description: 'Light Quality Good',
+    },
+    {
+      id: 'item11',
+      title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+      prices: {
+        'UniSize': 50.89,
+      },
+      photos: [
+        "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+        "https://api.slingacademy.com/public/sample-photos/3.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+      ],
+      description: 'Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch.',
+    },
+    {
+      id: 'item12',
+      title: 'Bicycle',
+      prices: {
+        'Red': 41.54,
+        'Blue': 32.12,
+        'Green': 65.02,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/9.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/10.jpeg",
+      ],
+      description: 'Bicycle perfect for little kids to use on a morning day.',
+    },
+    {
+      id: 'item13',
+      title: 'Hanna Montanas Album disc',
+      prices: {
+        'Old': 20.20,
+        'New': 40.40,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/7.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/6.jpeg",
+      ],
+      description: 'Last seen updated album on the perfect gift for the season for the children',
+    },
+    {
+      id: 'item14',
+      title: 'Wardrobe Cover',
+      prices: {
+        'UniSize': 340.01,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/3.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/8.jpeg",
+      ],
+      description: 'Cover for the wardbrobe in green tones.',
+    },
+    {
+      id: 'item15',
+      title: 'Tennis racket professional for special occasions ribbon included',
+      prices: {
+        'UniSize': 21.63,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/4.jpeg",
+      ],
+      description: 'As shown on title',
+    },
+    {
+      id: 'item16',
+      title: 'Almonds Package',
+      prices: {
+        'venti': 203.12,
+        'medium': 150.6,
+        'poco': 78.99,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/3.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+      ],
+      description: 'Organic gluten free almonds from amazonia, retrieve in the back of the gentle farmers all the way to your kitchen.',
+    },
+    {
+      id: 'item17',
+      title: 'Umbrella',
+      prices: {
+        'Academy': 12000.99,
+        'Corp': 242029.21,
+        'Song': 1.6,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/6.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/8.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/9.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/4.jpeg",
+      ],
+      description: 'All umbrella related items in our shop.',
+    },
+    {
+      id: 'item18',
+      title: 'Apple',
+      prices: {
+        'Apple': 9.99,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/7.jpeg",
+      ],
+      description: 'Apple.',
+    },
+    {
+      id: 'item19',
+      title: 'Orange',
+      prices: {
+        'Orange': 9.99,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/8.jpeg",
+      ],
+      description: 'Orange.',
+    },
+    {
+      id: 'item19',
+      title: 'Electrocardiogram machine EX',
+      prices: {
+        'EX': 12312.12,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/8.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/3.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/7.jpeg",
+      ],
+      description: 'Best electrocardiogram machine on stores, perfect for your hospital with the best insurance up to 10 years after purchase.',
+    },
+    {
+      id: 'item20',
+      title: 'Song of the day past',
+      prices: {
+        'Eng': 50.12,
+        'Esp': 50.12,
+        'Tuk': 50.12,
+      },
+      photos: [
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+        "https://api.slingacademy.com/public/sample-photos/2.jpeg",
+      ],
+      description: 'Song in different languages perfect for education.',
+    },
   ];
 }
--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { ItemT } from '../../app-data/items/item.type';
+import { ItemsService } from '../../app-data/items/item.service';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatListModule, MatIconModule, CommonModule],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.sass'
+  styleUrl: './item-list.component.sass',
 })
 export class ItemListComponent {
+  protected titleLabel: string = 'Items List';
+  protected contentLabel: string = 'Available items on the catalog';
+  protected listTitleLabel: string = 'Items';
+  protected items$: Observable<Array<ItemT>> = this.itemsService.items$;
 
+  public constructor(
+    private readonly itemsService: ItemsService,
+  ) {}
 }
